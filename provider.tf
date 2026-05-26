aa528987fc1e7fb38861447753421f7314e32514
kannanmadhu <kannanmadhu16@gmail.com>
2026-05-26 21:39:08 +0800

    trial 2

--- provider.tf
+++ provider.tf
@@ -1,11 +1,20 @@
 terraform {
+  required_version = ">= 1.5.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.8"
+    }
   }
 }
 
 # Configure the AWS Provider
-provider "aws" {}
+provider "aws" {
+  region = "ap-southeast-1"
+}
